43026bba2b7f31fdcb1cf33da5c57e6b85172dc8
George Rennie <georgerennie@gmail.com>
2022-05-19 17:34:20 +0100

    Complete fault trace ATPG

--- post_silicon/atpg/fault_cover.py
+++ post_silicon/atpg/fault_cover.py
@@ -1,4 +1,4 @@
-from typing import DefaultDict, List, Iterable, Tuple, Dict
+from typing import DefaultDict, List, Iterable, Tuple, Dict, Set
 from pysmt.shortcuts import Symbol, FreshSymbol, Not, Or
 from pysmt.solvers.solver import Model
 from collections import defaultdict
@@ -29,7 +29,8 @@ class FaultCover(Netlist):
             net for net in self.nets if self.name_from_net(net) in self.top.ports
         ]
 
-        self.covers: List[Tuple[Symbol, TransitionSystem, str]] = []
+        # Dict from name to cover
+        self.covers: Dict[str, Tuple[Symbol, TransitionSystem]] = {}
 
     def cover_observability(
         self, fault_ts: TransitionSystem, fault_nets: Iterable[Net], fault_name: str
@@ -60,8 +61,10 @@ class FaultCover(Netlist):
             # diverges
             observation_points.append(Not(self.symb_map[output].Iff(outer_output)))
 
-        self.covers.append(
-            (Or(observation_points), fan_out_system.map_symbols(subs), fault_name)
+        assert fault_name not in self.covers
+        self.covers[fault_name] = (
+            Or(observation_points),
+            fan_out_system.map_symbols(subs),
         )
 
     def cover_stuck_at(self, net: Net, value: bool):
@@ -75,31 +78,56 @@ class FaultCover(Netlist):
     def cover_io_bridge(self, net_1: Net, net_2: Net):
         symb_1, symb_2 = self.symb_map[net_1], self.symb_map[net_2]
         condition = Not(symb_1.Iff(symb_2))
-        self.covers.append(
-            (
-                condition,
-                TransitionSystem(),
-                f"diff({self.name_from_net(net_1)}, {self.name_from_net(net_2)})",
-            )
-        )
+        fault_name = f"bridge({self.name_from_net(net_1)}, {self.name_from_net(net_2)})"
+        assert fault_name not in self.covers
+        self.covers[fault_name] = (condition, TransitionSystem())
 
-    def get_trace(self, timeout: int = 5, start: int = 0) -> Tuple[Model, int]:
+    def get_trace(self, timeout: int = 20, start: int = 0) -> Tuple[Model, int]:
         covers: List[Symbol] = []
         cover_ts = self.ts
 
-        for cover, fault_ts, _ in self.covers:
+        for cover, fault_ts in self.covers.values():
             covers.append(cover)
             cover_ts += fault_ts
 
-        return CoverBMC(cover_ts).generate_trace(covers, start=start)
+        return CoverBMC(cover_ts).generate_trace(covers, timeout=timeout, start=start)
 
-    def generate_test_vectors(self, trace: Tuple[Model, int]):
+    def get_subset_trace(
+        self, cover_names: Iterable[str], timeout: int = 20, start: int = 0
+    ) -> Tuple[Model, int]:
+        covers: List[Symbol] = []
+        cover_ts = self.ts
+
+        for name in cover_names:
+            cover, fault_ts = self.covers[name]
+            covers.append(cover)
+            cover_ts += fault_ts
+
+        return CoverBMC(cover_ts).generate_trace(covers, timeout=timeout, start=start)
+
+    def get_short_covers(self, timeout: int = 5) -> Set[str]:
+        short_covers = set()
+        for cover_name, (cover, fault_ts) in self.covers.items():
+            print(f"Checking {cover_name}")
+            model, _ = CoverBMC(self.ts + fault_ts).generate_trace(
+                [cover], timeout=timeout, start=0
+            )
+            if model:
+                short_covers.add(cover_name)
+        return short_covers
+
+    def generate_test_vector_header(self) -> str:
+        header = "<PinDef>\n\t"
+        header += ",".join(map(self.name_from_net, self.ports))
+        header += "\n</PinDef>\n<TestVector>\n"
+        return header
+
+    def generate_test_vector_end(self) -> str:
+        return "</TestVector>"
+
+    def generate_test_vector_body(self, trace: Tuple[Model, int]) -> str:
         model, t = trace
-
-        test_vectors = "<PinDef>\n\t"
-        test_vectors += ",".join(map(self.name_from_net, self.ports))
-        test_vectors += "\n</PinDef>\n<TestVector>\n"
-
+        test_vectors = "\t# New trace\n"
         for i in range(t + 1):
             test_vectors += "\t"
             add_clock = False
@@ -125,49 +153,13 @@ class FaultCover(Netlist):
                         )
                         test_vectors += str(int(val))
                 test_vectors += "\n"
+        return test_vectors
 
-        test_vectors += "</TestVector>"
+    def generate_test_vectors(self, trace: Tuple[Model, int]) -> str:
+        test_vectors = self.generate_test_vector_header()
+        test_vectors += self.generate_test_vector_body(trace)
+        test_vectors += self.generate_test_vector_end()
         return test_vectors
 
     def sym(self, name: str) -> Symbol:
         return self.symb_map[self.net_from_name(name)]
-
-
-# fc = FaultCover(
-#     "design/TEAMF_DESIGN.json",
-#     # "design_small/TEAMF_DESIGN.json",
-#     "design/d2lib.json",
-#     # ["Q2", "Q3", "Q4", "Q5"],
-#     # ["Q17", "Q18", "Q19", "Q20", "Q21", "Q22", "Q23"],
-#     # ["Q6", "Q7"],
-#     # ["Q6"],
-#     # ["Q13"],
-#     ["Q8", "Q9", "Q10", "Q11", "Q12", "Q13", "Q14", "Q15", "Q16"],
-#     ["A13"],
-# )
-
-
-# fc.ts.add_init(Not(fc.sym("A14")))
-
-# print(len(fc.nets))
-# nets = list(fc.nets)
-
-# # for net in fc.nets:
-# # for i in range(20):
-# #     net = nets[i]
-# #     fc.cover_stuck_at(net, False)
-# #     fc.cover_stuck_at(net, True)
-
-# fc.cover_stuck_at(fc.net_from_name("TEAMF_CLOCK_SEQ_1.min10_0"), False)
-# fc.cover_stuck_at(fc.net_from_name("TEAMF_CLOCK_SEQ_1.min10_1"), False)
-# fc.cover_stuck_at(fc.net_from_name("TEAMF_CLOCK_SEQ_1.min10_2"), False)
-# fc.cover_stuck_at(fc.net_from_name("TEAMF_CLOCK_SEQ_1.min10_0"), True)
-# fc.cover_stuck_at(fc.net_from_name("TEAMF_CLOCK_SEQ_1.min10_1"), True)
-# fc.cover_stuck_at(fc.net_from_name("TEAMF_CLOCK_SEQ_1.min10_2"), True)
-
-# # fc.cover_stuck_at(fc.net_from_name("TEAMF_CLOCK_SEQ_1.N_56"), True)
-# # fc.cover_stuck_at(fc.net_from_name("TEAMF_CLOCK_SEQ_1.N_56"), False)
-
-# fc.generate_test_vectors(fc.get_trace(timeout=70, start=40))
-# with open("test.vec", "w") as f:
-#     f.write(fc.generate_test_vectors(fc.get_trace(timeout=70, start=40)))
---
+++ post_silicon/atpg/seq_test.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python3
+
+from pathlib import Path
+import itertools
+from typing import Iterable
+from fault_cover import FaultCover
+from pysmt.shortcuts import Not
+
+
+class D2SeqTest(FaultCover):
+    def __init__(self, test_name: str):
+        print(f"Creating test {test_name}")
+
+        super().__init__(
+            "design/TEAMF_DESIGN.json",
+            "design/d2lib.json",
+            ["Q8", "Q9", "Q10", "Q11", "Q12", "Q13", "Q14", "Q15", "Q16"],  # Outputs
+            ["A13"],  # Clock
+        )
+
+        # Start in reset
+        self.ts.add_init(Not(self.sym("A14")))
+
+        clk_rst = list(map(self.net_from_name, ["A13", "A14"]))
+
+        # Cover all stuck-ats that arent ties or clock/reset
+        for net in self.nets:
+            if net in clk_rst:
+                continue
+
+            tie0 = False
+            tie1 = False
+
+            if net in self.source_map:
+                cell_type = self.top.cells[self.source_map[net]].type
+                tie0 = cell_type == "TIE0"
+                tie1 = cell_type == "TIE1"
+
+            if not tie0:
+                self.cover_stuck_at(net, False)
+            if not tie1:
+                self.cover_stuck_at(net, True)
+
+        # Cover all bridges between I/O pins except clk/rst pins
+        for (a, b) in itertools.combinations(self.ports, 2):
+            self.cover_io_bridge(a, b)
+
+        short_covers = self.get_short_covers(timeout=5)
+        # print(short_covers)
+
+        print(f"{len(self.covers)} covers, {len(short_covers)} short covers")
+        print(f"{len(short_covers) / len(self.covers)} coverage")
+
+        test_vectors = self.generate_test_vector_header()
+
+        chunk_size = 15
+        chunks = list(zip(*(iter(short_covers),) * chunk_size))
+        for i, chunk in enumerate(chunks):
+            print(f"Covering chunk {i + 1} of {len(chunks)}")
+            trace = self.get_subset_trace(chunk, timeout=15, start=4)
+            test_vectors += self.generate_test_vector_body(trace)
+
+        test_vectors += self.generate_test_vector_end()
+
+        fault_dir = Path("fault_vectors")
+        fault_dir.mkdir(parents=True, exist_ok=True)
+
+        with open(fault_dir / Path(test_name + ".vec"), "w") as f:
+            f.write(test_vectors)
+
+
+D2SeqTest("seq_test")
