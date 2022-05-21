a9d380cf0a9a65d1929ec8dc5492be4f7758da64
George Rennie <georgerennie@gmail.com>
2022-05-21 16:00:27 +0100

    Remove exposed internal wires from sequencer_equiv

--- pre_silicon/sequencer/sequencer_equiv.sv
+++ pre_silicon/sequencer/sequencer_equiv.sv
@@ -6,21 +6,10 @@ wire [3:0] gold_D, gate_D;
 wire [3:0] gold_Digit, gate_Digit;
 wire       gold_DP, gate_DP;
 
-wire       gold_hour10, gate_hour10;
-wire [3:0] gold_hour1, gate_hour1;
-wire [2:0] gold_min10, gate_min10;
-wire [3:0] gold_min1, gate_min1;
-
 sequencer gold(
 	.D(gold_D),
 	.Digit(gold_Digit),
 	.DP(gold_DP),
-
-	.hour10(gold_hour10),
-	.hour1(gold_hour1),
-	.min10(gold_min10),
-	.min1(gold_min1),
-
 	.*
 );
 
@@ -34,12 +23,6 @@ TEAMF_CLOCK_SEQ_WRAPPER gate(
 	.Digit3(gate_Digit[1]),
 	.Digit4(gate_Digit[0]),
 	.DP(gate_DP),
-
-	.hour10(gate_hour10),
-	.hour1(gate_hour1),
-	.min10(gate_min10),
-	.min1(gate_min1),
-
 	.*
 );
 
@@ -47,11 +30,6 @@ always @* begin
 	assert(gold_D == gate_D);
 	assert(gold_Digit == gate_Digit);
 	assert(gold_DP == gate_DP);
-
-	assert(gold_hour10 == gate_hour10);
-	assert(gold_hour1 == gate_hour1);
-	assert(gold_min10 == gate_min10);
-	assert(gold_min1 == gate_min1);
 end
 
 endmodule
